a3ba2316a883604bd36fbaf6d7e7aeaf14c46531
Joseph D <adrian.de.vico@gmail.com>
2023-04-15 21:49:37 -0700

    Fixed working example for simple midi interface

--- midi_interface/midi_interface_basic/single_midi_in.v
+++ midi_interface/midi_interface_basic/single_midi_in.v
@@ -1,5 +1,3 @@
-
-
 module single_midi_in
 #(
     parameter BYTE_W = 8,               // std byte w
@@ -16,93 +14,92 @@ module single_midi_in
     output reg new_byte_strobe
 );
 
-    // Should be 768
-    //localparam CLK_PER_MIDI_BIT = (SYSCLK_F / MIDI_BAUD) / 2;
-    localparam CLK_PER_MIDI_BIT = 11'd768;
+    // Should be 1536 - 2
+    localparam CLK_PER_MIDI_BIT = (SYSCLK_F / MIDI_BAUD) - 2;
+    //localparam CLK_PER_MIDI_BIT = 11'd768;
 
 
-    // 384
-    //localparam HALF_BIT_PERIOD = CLK_PER_MIDI_BIT / 2;
-    localparam HALF_BIT_PERIOD = 11'd384;
+    // 768 - 1
+    localparam HALF_BIT_PERIOD = CLK_PER_MIDI_BIT / 2;
+    //localparam HALF_BIT_PERIOD = 11'd384;
 
     // Falling edge for start bit
-    localparam START_CONDITION = 2'b10;
+    localparam START_CONDITION = 3'b100;
 
-    reg [2:0]state;
+    //  Start + 8 Data, no parity. +1 for frameindic
+    localparam FRAME_SIZE = ( 1 + 8 + 1);
+
+    reg input_buffer;
+    reg [1:0]state;
     reg [10:0]clk_accumulator;
-    reg [1:0]start_bit_detector;
-    reg [9:0]frame_input;
+    reg [2:0]start_bit_detector;
+    reg [(FRAME_SIZE - 1):0]frame_input;
+    reg delay_line;
 
     initial begin
-        state = 3'b000;
-        data_rx = 8'h00;
-        is_command = 1'b0;
-        start_bit_detector = 2'b00;
-        clk_accumulator = {11{1'b0}};
-        frame_input = {10{1'b0}};
+        input_buffer    = 1'b0;
+        data_rx         = 8'h00;
+        is_command      = 1'b0;
         new_byte_strobe = 1'b0;
+
+        state           = 2'b00;
+        clk_accumulator = {11{1'b0}};
+        start_bit_detector = 3'b000;
+        frame_input     = {FRAME_SIZE{1'b0}};
+        delay_line = 1'b0;
     end
 
-/*
-    X---\___/---\___/---\___/---\___/---\___/---\___/
-    |   |   |   |   |   |   |   |   |   |   |   |   |
-        ST
 
-*/
-    // Start detector, "Baud rate generator"
     always @ (posedge sys_clk) begin
-        start_bit_detector <= {start_bit_detector[0], MIDI_IN};
-        
+        input_buffer <= MIDI_IN;
+        case(state)
+            0: begin // wait for start
+                start_bit_detector <= {start_bit_detector[1], start_bit_detector[0], input_buffer};
 
-        if((~|state) && start_bit_detector == START_CONDITION) begin
-            state <= 3'b001;
-            clk_accumulator <= {11{1'b0}};
-        end
-        //else begin
-        //    clk_accumulator <= clk_accumulator + 1;
-        //end
+                new_byte_strobe <= 1'b0;
+                is_command <= 1'b0;
+                delay_line <= 1'b0;
 
-        // Main state machine
-        case (state)
-            1: begin    // Stall for first 1.5 half periods
-                if(clk_accumulator >= (HALF_BIT_PERIOD + CLK_PER_MIDI_BIT)) begin
+                if(start_bit_detector == START_CONDITION) begin
+                    state <= state + 1;     
                     clk_accumulator <= {11{1'b0}};
-                    state <= state + 1;
-                    frame_input <= {MIDI_IN, 1'b1, 8'b00000000};
-                end
-                else begin
-                    clk_accumulator <= clk_accumulator + 1;
+                    frame_input <= {1'b1, {(FRAME_SIZE - 1){1'b0}}};
                 end
             end
 
-            2: begin    // Read bits
-                if(clk_accumulator >= (CLK_PER_MIDI_BIT)) begin
+            1: begin    // wait for half bit time offset
+                clk_accumulator <= clk_accumulator + 1;
+
+                if(clk_accumulator == HALF_BIT_PERIOD) begin
+                    state <= state + 1;
                     clk_accumulator <= {11{1'b0}};
-                    frame_input <= {MIDI_IN, frame_input[9:1]};
                 end
-                else begin
-                    clk_accumulator <= clk_accumulator + 1;
+            end
+
+            2: begin    // read in bits
+                clk_accumulator <= clk_accumulator + 1;
+
+                if(clk_accumulator == CLK_PER_MIDI_BIT) begin
+                    // 10 bit size [9:0], frame size == 10
+                    frame_input <= {input_buffer, frame_input[FRAME_SIZE - 1:1]};
+                    clk_accumulator <= {11{1'b0}};
                 end
 
                 if(frame_input[0]) state <= state + 1;
             end
 
-            3: begin
-                clk_accumulator <= {11{1'b0}};
-                state <= state + 1;
-
+            3: begin    // Post output
                 data_rx <= frame_input[8:1];
+                is_command <= frame_input[8];
                 new_byte_strobe <= 1'b1;
-
-                is_command <= frame_input[8]; 
+                delay_line <= 1'b1;
+                if(delay_line) begin
+                    state <= state + 1;
+                    start_bit_detector <= 3'b111;
+                end
             end
-
-            4: state <= state + 1;
-
-            5: begin
-                new_byte_strobe <= 1'b0;
-            end
-
         endcase
     end
+
+
 endmodule
